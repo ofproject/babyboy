00441289fd60d9e472408280661b38ed41f5a17e
zhengbin <zhengbin@ofbank.com>
2019-01-25 22:01:07 +0800

    Baby: Optimization code

--- config/errors.go
+++
@@ -1,9 +0,0 @@
-package config
-
-import "errors"
-
-var (
-	ErrInitDbFailed     = errors.New("init database error")
-	ErrInitP2PFailed    = errors.New("init P2P Server error")
-	ErrInitRpcSerFailed = errors.New("init RpcServer error")
-)
